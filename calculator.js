a0b9608e5fcc829be9a85525ce173df290188883
Abigail <abigaildelgado@berkeley.edu>
2022-03-14 15:00:43 -0700

    building input number function works for 4 and 6 as of now

--- calculator.js
+++ calculator.js
@@ -3,9 +3,11 @@
 // If it's clicked when there's only one digit, it sets the current number to be 0.
 
 //Global Scope Definitions
-let curr = 0; 
+let curr = 0; // our current total/result
+let edit = 0; // number user wants to add, multiply, etc. to current total
 const button = document.getElementById("five-button");
 const result = document.querySelector(".result-screen"); // result screen on calculator
+let currEdit = false; // True if user is still typing a number (more than 1 digit), False if result-screen is displaying curr
 
 button.addEventListener("click", add);
 
@@ -18,4 +20,19 @@ function add(){
 function updateTotal(toAdd){
     curr += toAdd;
     result.innerText = curr;
+}
+
+function updateEdit(int){
+    currEdit = true;
+    if(edit == 0){
+        edit = int;
+    } else {
+        strNum = edit.toString();
+        strInt = int.toString();
+        strNum = strNum.concat(strInt);
+
+        edit = parseInt(strNum);
+    }
+
+    result.innerText = edit;
 }
