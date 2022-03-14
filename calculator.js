2c04260aa2bae06cb6f4b68f723584a65931cb56
Abigail <abigaildelgado@berkeley.edu>
2022-03-14 14:43:27 -0700

    got number 5 button to add 5 to result screen with every click

--- calculator.js
+++ calculator.js
@@ -1,1 +1,21 @@
 //Check out calculator_hints.js for hints if you're stuck
+//It's like pressing backspace; it'll delete the last character typed. 
+// If it's clicked when there's only one digit, it sets the current number to be 0.
+
+//Global Scope Definitions
+let curr = 0; 
+const button = document.getElementById("five-button");
+const result = document.querySelector(".result-screen"); // result screen on calculator
+
+button.addEventListener("click", add);
+
+function add(){
+    strNum = button.textContent;
+    intNum = parseInt(strNum);
+    updateTotal(5);
+}
+
+function updateTotal(toAdd){
+    curr += toAdd;
+    result.innerText = curr;
+}
